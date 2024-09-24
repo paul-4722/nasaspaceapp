5ddf2fd7583f3834ca55bd6f5d2d943e5d10b37b
paul4722 <4722lego@gmail.com>
2024-09-24 16:01:09 +0900

    test6

--- server/server/settings.py
+++ server/server/settings.py
@@ -40,9 +40,9 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
