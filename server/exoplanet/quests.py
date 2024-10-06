ab820a789dce1b7fdd02a99e08ab5d6d5bd6cb62
paul-4722 <4722lego@gmail.com>
2024-10-06 21:26:04 +0900

    quest data

--- server/exoplanet/quests.py
+++ server/exoplanet/quests.py
@@ -1,19 +1,245 @@
 quests = [
     {
         "number": 1, 
-        "type": "mytype", 
-        "name": "quest1",
-        "description": "test quest. Do more coding.", 
-        "points": 10, 
-        "target": 10, 
+        "type": "selectStar", 
+        "name": "Beginning of the Galactic Empire",
+        "description": "Choose your own exoplanet system.", 
+        "points": 50, 
+        "target": 1, 
     }, 
     
     {
         "number": 2, 
-        "type": "mytype2", 
-        "name": "quest2",
-        "description": "test quest. Do more coding.", 
-        "points": 5, 
+        "type": "create", 
+        "name": "Customize One Planet",
+        "description": "Customize one planet in your stellar system.", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 3, 
+        "type": "create", 
+        "name": "Three's Company",
+        "description": "Customize three planet in your stellar system.", 
+        "points": 70, 
+        "target": 3, 
+    }, 
+    
+    {
+        "number": 4, 
+        "type": "createHome", 
+        "name": "Home Sweet Home",
+        "description": "Place at least one planet in the habitable zone.", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 5, 
+        "type": "createQuest", 
+        "name": "Cometlike Planet?",
+        "description": "Create a planet with a highly eccentric orbit. ", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 6, 
+        "type": "helpSemimajor", 
+        "name": "Orbit Help",
+        "description": "Go to 'Help!' and find the semi-major axis - stellar mass graph. ", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 7, 
+        "type": "helpClass", 
+        "name": "Planet Class Master",
+        "description": "Go to 'Help!' and learn the 15 planet classes (Temperature - Planet Radius Graph).", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 7, 
+        "type": "helpClass", 
+        "name": "Planet Class Master",
+        "description": "Go to 'Help!' and learn the 15 planet classes (Temperature - Planet Radius Graph).", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 8, 
+        "type": "createWarm", 
+        "name": "Solid H₂O, Liquid CO₂",
+        "description": "Set up planet parameters like solid H₂O and liquid CO₂.", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 9, 
+        "type": "createHot", 
+        "name": "Solid ZnS, Liquid H₂O",
+        "description": "Set up planet parameters like solid ZnS and liquid H₂O.", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 10, 
+        "type": "createCold", 
+        "name": "Solid CO₂, Liquid CH₄",
+        "description": "Set up planet parameters like solid CO₂ and liquid CH₄.", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 11, 
+        "type": "createR", 
+        "name": "Planet Types - R",
+        "description": "Create planet type - R", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 12, 
+        "type": "createE", 
+        "name": "Planet Types - E",
+        "description": "Create planet type - E", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 13, 
+        "type": "createS", 
+        "name": "Planet Types - S",
+        "description": "Create planet type - S", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 14, 
+        "type": "createN", 
+        "name": "Planet Types - N",
+        "description": "Create planet type - N", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 15, 
+        "type": "createJ", 
+        "name": "Planet Types - J",
+        "description": "Create planet type - J", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    
+    {
+        "number": 16, 
+        "type": "helpMassFlux", 
+        "name": "Flux-Radius Master",
+        "description": "Go to 'Help!' and find the Planet Flux - Planet Radius graph", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 17, 
+        "type": "helpMassSemimajor", 
+        "name": "Mass-Semimajor Master",
+        "description": "Go to 'Help!' and find the Oribital Distance - Planet Radius graph", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 18, 
+        "type": "createGold", 
+        "name": "The Goldilocks Zone",
+        "description": "Keep the surface temperature between 273K and 373K by adjusting the albedo (water can remain in liquid form).", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 19, 
+        "type": "helpESI", 
+        "name": "ESI Explorer",
+        "description": "Go to 'Help!' and learn about the ESI index.", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 20, 
+        "type": "createESI", 
+        "name": "High ESI Planet",
+        "description": "Keep the ESI of your planet greater than 0.8.", 
+        "points": 100, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 21, 
+        "type": "createMagnetic", 
+        "name": "Magnetic Field vs. Rotation",
+        "description": "Set the magnetic field and rotation speed inversely proportional to each other.", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 22, 
+        "type": "helpTempEsc", 
+        "name": "Escape Velocity Explorer",
+        "description": "•	Escape Velocity Explorer", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 23, 
+        "type": "explore", 
+        "name": "Pathfinder", 
+        "description": "Explore one user’s solar system. ", 
+        "points": 50, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 24, 
+        "type": "explore", 
+        "name": "Pro-Travler", 
+        "description": "Explore ten user’s solar system. ", 
+        "points": 70, 
         "target": 10, 
+    }, 
+    
+    {
+        "number": 25, 
+        "type": "exploreTry", 
+        "name": "Review", 
+        "description": "Try the first quiz", 
+        "points": 70, 
+        "target": 1, 
+    }, 
+    
+    {
+        "number": 26, 
+        "type": "exploreCorrect", 
+        "name": "Genius", 
+        "description": "Solve the first quiz", 
+        "points": 100, 
+        "target": 1, 
     }, 
 ]
